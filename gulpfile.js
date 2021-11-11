865833792b9045d34bbce2acb7cf875ee60b4f00
Shishir Neupane <shishirs520@gmail.com>
2021-11-11 13:21:37 +0545

    image optimization with 'gulp-image'

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,40 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
+const image = require('gulp-image');
+
+
+// minify images
+gulp.task('image', function() {
+  return gulp.src('img/*')
+    .pipe(image())
+    .pipe(gulp.dest('dist'));
+});
+
 
 // compile SCSS to CSS
 gulp.task('sass', function () {
-  return gulp.src('scss/*.scss')
+  return gulp.src('*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist'));
-})
+    .pipe(gulp.dest('.'));
+});
+
 
 // minify CSS
 gulp.task('css', function () {
-  return gulp.src('dist/*.css')
+  return gulp.src('*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist/min'));
+    .pipe(gulp.dest('dist'));
 });
+
 
 // minify HTML
 gulp.task('html', function () {
   return gulp.src('*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist/min'));
+    .pipe(gulp.dest('dist'));
 });
+
+
+// run all gulp tasks
+gulp.task('develop', gulp.series('image', 'sass', 'css', 'html'));
